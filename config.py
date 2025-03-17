8ab8d4611e0b47651a83af27e5e324588ba8b589
cinerin-team <cinerin@mail.com>
2025-03-17 15:26:56 +0100

    hibás tábla exception

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # these variables should be updated!!!
-UPDATE_INTERVAL = 30000 # in millisecond
+UPDATE_INTERVAL = 900000 # in millisecond
 
 # these variables should not be updated
 TODO_DASHBOARD = "https://dashboards.sero.wh.rnd.internal.ericsson.com/epg_st_passrates/epg_st_passrates_weekly_master_to_do_team.html"
